7a5f515f1e94719ed1afd5cd7f9076117d18e030
Oakleigh <oakleighweekes@hotmail.com>
2022-04-20 13:30:36 +0100

    finished pooling solution

--- pool_workflow.py
+++ pool_workflow.py
@@ -6,38 +6,40 @@ if __name__ == "__main__":
     #family directory to be pooled
     cwd = os.getcwd()
     orgDirs = os.listdir(cwd)
-    #orgDirs = [x for x in orgDirs if x[-2:] != "py"]
 
-    #videos need to be in the format (annotations folder, images folder)
-    ###continuous dev notes###
-    #this code now pools images into one folder. need to format new Json accordingly (use dictionary to new image name?) Json creation is currently commented out
+    #video annotation directories need to be in the format (annotations folder, images folder)
 
-    #insert parent dir here (e.g. parent/   of video 1 folder, video 2 folder)
-    parent = r"C:\Users\Computing\Documents\annotationDump\testDir"
+    ########insert parent directory here (e.g. parent/   of video 1 folder, video 2 folder,....)########
+    #####video annotation directories need to be in the format (annotations folder, images folder)######
+    parent = r"C:\Users\Computing\Documents\annotationDump\testDir" #insert parent folder here
+
+    if (parent == None) | (len(parent) == 0):
+        print("Please add parent folder PATH of annotations within this code and run again")
+        quit()
+    ####################################################################################################
 
     orgDirs = os.listdir(parent)
     orgDirs = [(parent + "\\" + x + "\\annotations") for x in orgDirs]
-    # orgDirs = [
-    #     r"D:\datasets\greenVerges\annotationsDump\testDir2\day2\annotations",
-    #     r"D:\datasets\greenVerges\annotationsDump\testDir2\day3\annotations"
-    # ]
 
-    #this will make sure every image saved has a unique image ID regardless of the directory
-    globalCount = 0
-
-    #number of images in total, increases on every directory
+    #total directories
+    totalDirs = 0
+    #number of images in total, increases on every directory. This will make sure every image saved has a unique image ID regardless of the directory
     totalImgs = 0
 
     jsonsConnected = [] # contains jsons with items at relevant index (json 1 has images 0...N, json 2 has images N..... N+n)
 
     for direc in orgDirs:
         #get name of json
-        d = os.listdir(direc)
+        try:
+            d = os.listdir(direc)
+        except:
+            print("Error: There may be a pooledImage folder or extra json in the parent folder.\nPlease make sure that there are only directories containing an annotations and images folder each")
+            quit()
+
         jsons = []
         for file in d:
             if (file.split('.')[1] == 'json'):
                 jsons.append(file)
-        print(jsons)
     
         #only include one json in folder
         if len(jsons) > 1:
@@ -77,7 +79,9 @@ if __name__ == "__main__":
             noLength = len(strFrame)
             frameImgName = templateName[0:-4-noLength] + strFrame + templateName[-4:]
             frameImgs.append(frameImgName)
-    
+
+        #remove duplicates
+        frameImgs = list(set(frameImgs))
         ###Create new COCO JSON###
 
         #find indices of redundant (non-annotated) image data
@@ -118,7 +122,6 @@ if __name__ == "__main__":
             newFrameImgs.append(frameImgName)
             count +=1
 
-        print("debug1:", len(data['images']))
         j = 0
         #change frame names in json
         for i in data['images']:
@@ -126,19 +129,19 @@ if __name__ == "__main__":
             j += 1
 
         count = totalImgs + 1
+
         #change image id ('image_id' in annotations)
         for i in data['annotations']:
-            i['image_id'] = idToNew.get(i['image_id'])
+            i['image_id'] = idToNew.get(i['image_id']) #changes according to "old" dictionary key
             count += 1
 
         #increment totalImgs
         totalImgs += len(data['images'])
 
         #add new formed json to list
-        
         jsonsConnected.append(data)
 
-        ###Reduce images to only annotated ones (new folder)###
+        ###Reduce images to only annotated ones###
 
         pooledPath = os.path.dirname(os.path.dirname(direc))
         #copy images with annotations in into another folder
@@ -149,11 +152,6 @@ if __name__ == "__main__":
         sourceFolder = os.path.dirname(direc) + '\\images'
         destFolder = pooledPath + '\\pooledImages'
 
-        # #first check if folder is empty- we don't want to copy more than once!
-        # if (len(os.listdir(destFolder)) != 0):
-        #     print('Destination Folder already contains images. \n Please delete them first!')
-        #     quit()
-        #print("len(frameImgs))   error here
         for i in range(0,len(frameImgs)):
 
             source = sourceFolder + '/' + frameImgs[i]
@@ -161,37 +159,38 @@ if __name__ == "__main__":
             newFrameNo = newFrameImgs[i]
             destination = destFolder + '/' + newFrameNo
 
-            # print(source)
-            # print(destination)
             if os.path.isfile(source):
                 shutil.copy(source,destination) #copy images to filtered folder
-        globalCount += 1
+
+        #increment directories sorted
+        totalDirs += 1
 
     #get total images and annotations array of dictionaries
     imgs = []
     annots = []
     
     for i in range(0, len(jsonsConnected)):
-        print(i, "amount images:", len(jsonsConnected[i]['images']))
-        print(i, "amount annotations:", len(jsonsConnected[i]['annotations']))
         imgs += jsonsConnected[i]['images']
         annots += jsonsConnected[i]['annotations']
 
-    print(totalImgs)
-    print(len(imgs))
+
+
     #use first json as template for joinedJsons
     newJson = jsonsConnected[0]
     newJson['images'] = imgs
     newJson['annotations'] = annots
 
-    print(len(newJson['images']))
-    print(len(newJson['annotations']))
+    print(totalDirs,"directories pooled")
+    print(len(newJson['images']), "images found")
+    print(len(newJson['annotations']),"annotations found")
+
     #dump new json
-    newJsonName = parent + '\\' + 'reducedCOCO.json'
+    newJsonName = parent + '\\' + 'pooledCOCO.json'
     with open(newJsonName, 'w') as f:
             json.dump(newJson, f, indent= 1) #indent needs to be changed to reflect COCO #,indent = None
 
-    print("Reworked JSON file created in folder")
+    print("Reworked JSON file created in",  parent)
+    print("Annotated images copied to: " + parent + "\\pooledImages")
         
 
 
